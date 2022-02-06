ca672dfb9c6ad41e1cb283fec69aab2211474f3b
unknown <pandeybhuwan138@gmail.com>
2022-02-06 16:50:50 +0545

    First Phase

---
+++ src/emart/dao/EmployeesDoa.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package emart.dao;
+
+import emart.dbutil.DBConnection;
+import emart.pojo.EmployeesPojo;
+import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Bhuwan Pandey
+ */
+public class EmployeesDoa {
+    //to generate employeeId
+
+    /**
+     *
+     * @return
+     * @throws SQLException
+     */
+    public static String getEmployeeId() throws SQLException{
+        Connection conn  = DBConnection.getConnection();
+        Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery("select max(empid) from employees");
+        rs.next();
+        String empId = rs.getString(1); 
+        int empNo = Integer.parseInt(empId.substring(1));
+        empNo++;
+        return "E" + empNo;
+    }
+    
+     //to add employee
+    public static boolean addEmployee(EmployeesPojo addEmp) throws SQLException{
+        Connection conn = DBConnection.getConnection();
+        PreparedStatement ps = conn.prepareStatement("insert into employees values(?,?,?,?)");
+        ps.setString(1, addEmp.getEmployeeId());
+        ps.setString(2, addEmp.getEmployeeName());
+        ps.setString(3, addEmp.getJob());
+        ps.setDouble(4, addEmp.getSalary());
+        int result = ps.executeUpdate();
+        return result == 1;
+    }
+}
---
+++ src/emart/dao/UserDoa.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package emart.dao;
+
+import emart.dbutil.DBConnection;
+import emart.pojo.EmployeesPojo;
+import emart.pojo.UserPojo;
+import emart.pojo.UserProfile;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Bhuwan Pandey
+ */
+public class UserDoa {
+    
+    //checking for user id and pass to validate user
+    public static boolean validateUser(UserPojo user)throws SQLException{
+        Connection conn = DBConnection.getConnection();
+        PreparedStatement ps = conn.prepareStatement("select * from users where userid=? and password=? and usertype=?");
+        ps.setString(1, user.getUserId());
+        ps.setString(2, user.getPassword());
+        ps.setString(3, user.getUserType());
+        ResultSet rs = ps.executeQuery();
+        if(rs.next()){
+            String userName = rs.getString(5);
+            UserProfile.setUserName(userName);
+            return true;
+        }
+        return false;
+    }
+    
+   
+}
